6b380f8be26828123a02125cca3947014b200f70
Sagar Patel <sagarpatel.exe@gmail.com>
2016-02-16 13:19:16 +0530

    done

---
+++ README.md
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+# Flask to Azure Blob 
+=======
+# Uploading file from Python-Flask to Azure Blob 
+>>>>>>> 629e394136b66c45e70755039954f28d749b3985
+A simple Python flask application to upload files to Azure Blob
+
+**Steps**
+
+1. Go to Azure management portal and create a Storage Account
+2. Find the api access key from the azure storage dashboard
+3. Copy paste the account name and api key in the python script
+4. Write a container name of your wish
+5. Run the requirements file to install the required dependencies
+6. Run the python flask app locally to get access no the upload page
+
+Now you can select any file from your system and upload to the designated Azure Blob. Once the upload is completed, the system will return a url for future access.
+
+__Note__ - Larger files may take longer to upload
---
+++ blob.py
@@ -0,0 +1,52 @@
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+from azure.storage.blob import BlobService
+import string, random, requests
+
+app = Flask(__name__, instance_relative_config=True)
+
+app.config.from_pyfile('config.py')
+account = app.config['ACCOUNT']   # Azure account name
+key = app.config['STORAGE_KEY']      # Azure Storage account access key  
+container = app.config['CONTAINER'] # Container name
+
+blob_service = BlobService(account_name=account, account_key=key)
+blob_service.create_container(container, x_ms_blob_public_access='container')
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+    	file = request.files['file']
+    	filename = secure_filename(file.filename)
+    	fileextension = filename.rsplit('.',1)[1]
+        Randomfilename = id_generator()
+        filename = Randomfilename + '.' + fileextension
+        try:
+            blob_service.put_block_blob_from_file(container, filename, file,)
+        except Exception:
+            print 'Exception=' + Exception 
+            pass
+        ref =  'http://'+ account + '.blob.core.windows.net/' + container + '/' + filename
+        return '''
+	    <!doctype html>
+	    <title>File Link</title>
+	    <h1>Uploaded File Link</h1>
+	    <p>''' + ref + '''</p>
+	    <img src="'''+ ref +'''">
+	    '''
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+if __name__ == '__main__':
+    app.run(debug=True)
